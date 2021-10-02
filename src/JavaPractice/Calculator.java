f629cec4ebb5c7688d2b25401d409dad3564fcee
JonelSV <svjonel@gmail.com>
2021-10-02 19:48:02 -0400

    first commit

---
+++ src/JavaPractice/Calculator.java
@@ -0,0 +1,21 @@
+package JavaPractice;
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		char operator;
+		
+		double numberInput1, numberInput2;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter the operation you want to be performed (+, -, *, /): ");
+		
+		
+		
+	
+	}
+
+}
