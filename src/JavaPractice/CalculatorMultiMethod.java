b288027282091d6de7f9dc5b8f65332ac7353518
JonelSV <svjonel@gmail.com>
2021-10-02 20:31:42 -0400

    create calculator version 2

---
+++ src/JavaPractice/CalculatorMultiMethod.java
@@ -0,0 +1,16 @@
+package JavaPractice;
+import java.util.Scanner;
+
+public class CalculatorMultiMethod {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+	
+		
+		System.out.println("Enter the operation you want to perform( +, -, *, /):");
+		
+		int userInput = scan.nextInt();
+	}
+
+}
