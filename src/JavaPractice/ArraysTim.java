d0c6572fa12c16ae47b0dc12dbcc4161c88e3395
JonelSV <svjonel@gmail.com>
2021-10-04 17:52:32 -0400

    Arrays practice

---
+++ src/JavaPractice/ArraysTim.java
@@ -0,0 +1,22 @@
+package JavaPractice;
+
+// integer array, 10 is number of elements assigned in the array
+
+
+public class ArraysTim {
+
+	public static void main(String[] args) {
+		
+		
+		int[] myIntArray = new int[10];
+		myIntArray[5] = 33; 
+		
+		double[] myDoubleArray = new double[5];
+		myDoubleArray[3] = 50.22;
+		
+		
+		System.out.println(myIntArray[7]);
+		System.out.println(myDoubleArray[3]);
+	}
+
+}
