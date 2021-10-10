061a1dbbc6460a15ed3fb76df2e0233539a68c53
JonelSV <svjonel@gmail.com>
2021-10-09 23:26:21 -0400

    car class

---
+++ src/JavaPracticeInheritanceCars/Car.java
@@ -0,0 +1,11 @@
+package JavaPracticeInheritanceCars;
+
+public class Car extends Vehicle {
+	private int wheels;
+	private int doors; 
+	private int gears;
+	private int boolean isManual;
+	
+	private int currentGear;
+
+}
