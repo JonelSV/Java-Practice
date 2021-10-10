0387cf75b5778c944052f5d7e1c1d3e5b53b0b58
JonelSV <svjonel@gmail.com>
2021-10-09 23:27:41 -0400

    constructors created

--- src/JavaPracticeInheritanceCars/Car.java
+++ src/JavaPracticeInheritanceCars/Car.java
@@ -8,4 +8,13 @@ public class Car extends Vehicle {
 	
 	private int currentGear;
 
+	public Car(String name, String size, int wheels, int doors, int gears, boolean isManual, int currentGear) {
+		super(name, size);
+		this.wheels = wheels;
+		this.doors = doors;
+		this.gears = gears;
+		this.isManual = isManual;
+		this.currentGear = currentGear;
+	}
+
 }
