68e12a486a3097e2692b4d8e6675c62edbcd753e
JonelSV <svjonel@gmail.com>
2021-10-09 23:21:20 -0400

    commit

--- src/JavaPracticeInheritanceCars/Vehicle.java
+++ src/JavaPracticeInheritanceCars/Vehicle.java
@@ -5,10 +5,29 @@ public class Vehicle {
 	private String name;
 	private String size;
 	
+	private int currentVelocity;
+	private int currentDirection;
+	
 	
 	
 	public Vehicle (String name, String size) {
 		this.name = name;
 		this.size = size;
+		
+		this.currentDirection = 0;
+		this.currentVelocity = 0;
+	}
+	
+	public void Steer (int direction) {
+		
+		this.currentDirection = direction;
+		System.out.println("Vehicle.steer(): Steering at" + currentDirection + " degrees");
+		
+	}
+	
+	public void move (int velocity, int direction) {
+		currentVelocity = velocity;
+		currentDirection = direction;
+		System.out.println("Vehicle.move() : Moving at" + currentVelocity + " in Direction "+ currentDirection);
 	}
 }
