426ad692bc853e516c20caf77d3ff07a7583a923
JonelSV <svjonel@gmail.com>
2021-10-09 23:15:56 -0400

    inheritance vehicle practice

---
+++ src/JavaPracticeInheritanceCars/Vehicle.java
@@ -0,0 +1,14 @@
+package JavaPracticeInheritanceCars;
+
+public class Vehicle {
+
+	private String name;
+	private String size;
+	
+	
+	
+	public Vehicle (String name, String size) {
+		this.name = name;
+		this.size = size;
+	}
+}
