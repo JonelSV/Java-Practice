afb85df7d74395547b0281df65ab2198217022c8
JonelSV <svjonel@gmail.com>
2021-10-12 08:47:07 -0400

    inheritance car exercise

--- src/JavaPracticeInheritanceCars/Car.java
+++ src/JavaPracticeInheritanceCars/Car.java
@@ -4,21 +4,33 @@ public class Car extends Vehicle {
 	private int wheels;
 	private int doors; 
 	private int gears;
-	private int boolean isManual;
+	private boolean isManual;
 	
 	private int currentGear;
 
-	public Car(String name, String size, int wheels, int doors, int gears, boolean isManual, int currentGear) {
+	public Car(String name, String size, int wheels, int doors, int gears, boolean isManual) {
 		super(name, size);
 		this.wheels = wheels;
 		this.doors = doors;
 		this.gears = gears;
 		this.isManual = isManual;
-		this.currentGear = currentGear;
+		this.currentGear =1;
 	}
 	
-	public void carUse(String drive, int miles, int speed  ) {
-		System.out.println("try
+	public void changeGear(int currentGear) {
+		this.currentGear = currentGear;
+		System.out.println("Car.setCurrentGear() : Changed to " + this.currentGear + "gear.");
+		
+	}
+	
+	public void  changeVelocity( int speed, int direction ) {
+		move(speed, direction);
+		System.out.println("Car.changeVelocity() : Velocity " + speed + "direction" + direction);
+		
+	}
+	
+	public void stop () {
+		
 	}
 
 }
