a5749e2460fdaf9435065dfa205d061bf8cdc789
JonelSV <svjonel@gmail.com>
2021-10-13 20:48:53 -0400

    first commit

---
+++ src/ArraysExerciseTim/Main.java
@@ -0,0 +1,20 @@
+package ArraysExerciseTim;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		private static Scanner scanner = new Scanner(System.in);
+		
+		
+		
+		int [] myIntegers = getIntegers(5);
+	}
+
+	
+	public static int[] getIntegers(int capacity) {
+		int[] array = new int [capacity];
+		System.out.println("Enter " + capacity + " integer valuse : \r");
+	}
+}
---
+++ src/JavaPracticeInheritanceCars/Main.java
@@ -0,0 +1,17 @@
+package JavaPracticeInheritanceCars;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Outlander outlander = new Outlander(36, 0, 0, false);
+		outlander.Steer(45);
+		outlander.accelerate(30);
+		outlander.accelerate(20);
+		outlander.accelerate(-42);
+		
+		
+		
+	}
+
+}
